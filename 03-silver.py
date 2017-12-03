934a518624bc32d1b3c1d31e048dcd8ad7ab7752
TommyWithATiger <halvard.hummel@hotmail.com>
2017-12-03 09:04:35 +0100

    Removed random newline

--- 03-silver.py
+++ 03-silver.py
@@ -1,2 +1,1 @@
-print(sum(abs(sum(__import__("math").sin((__import__("math").floor((4*(n-2)+1)**0.5)%4)*(__import__("math").pi/2))for n in range(2,i+1))//1)+abs(sum(__import__("math").cos((__import__("math").floor((4*(n-2)+1)**0.5)%4)*(__import__("math").pi/2))for n in range(2,i+1))//1
-)for i in[int(__import__("sys").stdin.readline().strip())]))
+print(sum(abs(sum(__import__("math").sin((__import__("math").floor((4*(n-2)+1)**0.5)%4)*(__import__("math").pi/2))for n in range(2,i+1))//1)+abs(sum(__import__("math").cos((__import__("math").floor((4*(n-2)+1)**0.5)%4)*(__import__("math").pi/2))for n in range(2,i+1))//1)for i in[int(__import__("sys").stdin.readline().strip())]))
