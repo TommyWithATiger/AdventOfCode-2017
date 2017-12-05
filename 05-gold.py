14ca377159282bf017980c5912ef023321351a7a
TommyWithATiger <halvard.hummel@hotmail.com>
2017-12-05 06:45:37 +0100

    Finished the challenges for the fifth day

---
+++ 05-gold.py
@@ -0,0 +1,1 @@
+while 1:('x' in locals()or locals().update({'x':[*map(int,map(str.strip,__import__("sys").stdin.readlines()))],'s':0,'i':0}or 1))and(not(-1<i<len(x))and(print(s)or __import__("sys").exit(0)))or locals().update({'x':x[:i]+[x[i]+(-1)**(x[i]>2)]+x[i+1:],'s':s+1,'i':i+x[i]})
